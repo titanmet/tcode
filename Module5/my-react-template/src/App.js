c52dc7c596a5ec3e5eeff5e44fb7e9f11a709a52
andrei.ratnikov <andrei.ratnikov@simbirsoft.com>
2022-08-07 09:41:23 +0300

    Header Footer

--- Module5/my-react-template/src/App.js
+++ Module5/my-react-template/src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className="App">
-      <p>template</p>
-    </div>
-  );
-}
+import React from "react";
+
+import "./scss/main.scss";
+
+import { Header, Footer } from "./components";
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <Header />
+            <p>template</p>
+            <Footer />
+        </div>)
+};
 
 export default App;
