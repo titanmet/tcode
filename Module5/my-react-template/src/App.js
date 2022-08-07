4a283317c0568dc032be08c9f79673099a5b692b
andrei.ratnikov <andrei.ratnikov@simbirsoft.com>
2022-08-07 20:28:44 +0300

    Validate props

--- Module5/my-react-template/src/App.js
+++ Module5/my-react-template/src/App.js
@@ -2,17 +2,90 @@ import React from "react";
 
 import "./scss/main.scss";
 
+import { FaBeer } from 'react-icons/fa';
+
 import {Header, Footer} from "./components";
 import {Container} from "./layouts";
 
 const App = () => {
     return (
         <div className="ui-wrapper">
-            <Header/>
+            <Header isLogo />
             <div className="ui-content-wrapper">
                 <div className="ui-container">
                     <Container>
-                        <p className="text-2xl">template</p>
+                        <p>
+                            <br />
+                            <br />
+                            Aruana, "trahira, bandfish luminous hake staghorn sculpin clownfish." King-of-the-salmon swordtail,
+                            blue gourami scissor-tail rasbora razorfish. Tidewater goby carpsucker slipmouth; grideye lancetfish
+                            sea toad gombessa, Bombay duck flatfish mud cat.
+                            <br />
+                            <br />
+                            Pacific cod false trevally; large-eye bream demoiselle Red whalefish Blind shark boga alligatorfish
+                            skipjack tuna. Snubnose eel lamprey gizzard shad knifefish Ganges shark longnose lancetfish ponyfish
+                            orangespine unicorn fish velvetfish gar tenpounder northern clingfish.
+                            <br />
+                            <br />
+                            Buffalofish hagfish yellowtail barracuda sawtooth eel. Snook platy queen danio, waryfish--bichir:
+                            firefish whitebait; slender mola North American darter false moray. Mud catfish great white shark
+                            sockeye salmon forehead brooder. Morwong round whitefish jawfish goldfish African glass catfish.
+                            Sea raven velvetfish blue shark: Japanese eel flagfish mrigal, Australasian salmon longjaw mudsucker
+                            bullhead shark. Pickerel oilfish clown triggerfish cod, Colorado squawfish, dragonfish ropefish limia
+                            bluefish shortnose sucker aruana: sand lance common tunny.
+                            <br />
+                            <br />
+                            Queen triggerfish, inconnu Mexican blind cavefish slimy sculpin blacktip reef shark ribbonbearer
+                            butterflyfish. Trumpeter dwarf loach saw shark mummichog porgy flagtail sabertooth fish
+                            yellow-and-black triplefin whiting scabbard fish electric knifefish elephant fish, blue danio.
+                            Garden eel, naked-back knifefish round stingray; Antarctic cod snake eel sheatfish. European chub
+                            angelfish Alaska blackfish moray eel lined sole thornfish fire bar danio southern grayling.
+                            Northern clingfish requiem shark Black mackerel, molly loosejaw. Hake spotted danio sand tilefish.
+                            <br />
+                            <br />
+                            Thorny catfish white marlin barred danio deep sea eel threadtail! Warty angler demoiselle ronquil
+                            sea bass cusk-eel southern hake triplespine bigmouth buffalo. Smalltooth sawfish wahoo, lungfish
+                            spearfish grass carp elephant fish, lined sole: glassfish candiru. Scaly dragonfish weeverfish
+                            mummichog; man-of-war fish ribbonbearer stingfish Australian lungfish, gouramie bamboo shark bigscale
+                            fish. Round stingray--opah sunfish sawtooth eel, "houndshark cobbler roanoke bass scissor-tail rasbora
+                            dealfish Billfish," giant sea bass Port Jackson shark shortnose sucker round stingray. Mola
+                            ribbonbearer king-of-the-salmon firefish beluga sturgeon anglerfish weever beaked sandfish lined sole
+                            northern pearleye.
+                            <br />
+                            <br />
+                            Does your lorem ipsum text long for something a little fishier? Give our generator a try… it’s fishy!
+                            <br />
+                            <br />
+                            Manefish guppy Old World knifefish yellow perch." Unicorn fish cardinalfish: mudsucker southern
+                            grayling Hammerjaw round stingray. Pipefish deepwater stingray, longnose chimaera antenna codlet
+                            eel-goby kissing gourami basslet, thorny catfish Billfish paradise fish buffalofish flyingfish, Black
+                            angelfish. Sandroller duckbill eel river loach sauger hamlet splitfin, escolar yellow-edged moray
+                            sleeper cuskfish wels catfish gopher rockfish ocean perch pufferfish. Longfin escolar Black swallower
+                            barred danio oilfish snubnose parasitic eel blue catfish; yellow-and-black triplefin bluefin tuna
+                            Atlantic salmon, alfonsino, mustache triggerfish." Smalltooth sawfish; longfin dragonfish Atlantic
+                            herring frogmouth catfish labyrinth fish, plaice grouper. Beaked sandfish, "tailor thornyhead
+                            yellowfin cutthroat trout lionfish loach catfish velvet-belly shark, lookdown catfish New World
+                            rivuline." Bobtail snipe eel shad olive flounder barfish.
+                            <br />
+                            <br />
+                            Grenadier sergeant major snook Old World rivuline Redfish, sergeant major pupfish medaka triggerfish
+                            squaretail; burbot sarcastic fringehead halfbeak minnow spiny-back dwarf loach. Fire bar danio
+                            longnose whiptail catfish. Brotula lionfish Port Jackson shark Chinook salmon elver jack spaghetti
+                            eel Lost River sucker. Brotula Owens pupfish porgy riffle dace, pupfish Death Valley pupfish Atlantic
+                            trout peladillo trunkfish elver mahseer. Torrent catfish spearfish roanoke bass; Black
+                            pickerel--featherfin knifefish freshwater eel? Redtooth triggerfish pumpkinseed, ballan wrasse northern
+                            pearleye: electric knifefish sand tilefish filefish. Butterflyfish lake whitefish barbeled dragonfish
+                            ghost carp horsefish Antarctic cod freshwater shark snake mudhead parrotfish crappie.
+                            <br />
+                            <br />
+                            Cuskfish loach catfish javelin lagena stream catfish redside soldierfish lake whitefish Lost River
+                            sucker sheepshead; tuna spiny eel. Algae eater ghost flathead? Escolar tommy ruff Atlantic eel
+                            California flyingfish barbeled houndshark, ghost carp inconnu dwarf loach sawfish smalltooth sawfish.
+                            Half-gill barreleye mola tubeblenny carpetshark blue catfish Old World rivuline fingerfish.
+                            Long-finned char hardhead catfish Rabbitfish conger eel Siamese fighting fish Moorish idol; pilchard
+                            chimaera seatrout. Sandburrower Reedfish flashlight fish mudfish ribbonfish Cornish Spaktailed Bream
+                            catfish mola Black angelfish combtail gourami.
+                        </p>
                     </Container>
                 </div>
             </div>
--- Module5/my-react-template/src/components/Header/Header.jsx
+++ Module5/my-react-template/src/components/Header/Header.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 
-import Container from "../../layouts/Container"
+import propTypes from "prop-types";
 
-const Header = () => {
+import Container from "../../layouts/Container";
+
+import { FaBuysellads } from 'react-icons/fa';
+
+import './Header.scss';
+
+import {ReactComponent as LogoIcon} from "../../assets/img/logo.svg"
+
+const Header = ({isLogo, isFixed}) => {
     return (
         <Container>
-            <div className="header">
-                <p>header</p>
+            <div className={`Header flex justify-between py-2 mb-4 ${isFixed && "isFixed"}`}>
+                <div className="Logo">
+                    {isLogo && <LogoIcon />}
+                    <span>React app</span>
+                </div>
+                <ul className="ui-button-group">
+                    <li className="ui-link">Home</li>
+                    <li className="ui-link">About</li>
+                </ul>
             </div>
         </Container>)
 };
+
+Header.propTypes = {
+    isLogo : propTypes.bool,
+    isFixed : propTypes.bool
+}
+
+Header.defaultProps = {
+    isLogo: false,
+    isFixed: false
+}
 
 export default Header;
