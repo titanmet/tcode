ab428f444475a67981a53f58e654e4e9eb438d84
andrei.ratnikov <andrei.ratnikov@simbirsoft.com>
2022-08-08 08:48:29 +0300

    classNames

--- Module5/my-react-template/src/components/Footer/Footer.jsx
+++ Module5/my-react-template/src/components/Footer/Footer.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import Container from "../../layouts/Container";
 
-const Footer = () => {
+const Footer = ({...attrs}) => {
     return (
-        <Container>
-        <div className="footer">
-            <p>footer</p>
-        </div>
-        </Container>)
+        <footer className="Footer" {...attrs}>
+            <Container>
+                <div className="Footer py-2 mt-6">
+                    <p className="text-center text-xs" style={{opacity: 0.6}}>Сделано A.P.</p>
+                </div>
+            </Container>
+        </footer>)
 };
 
 export default Footer;
--- Module5/my-react-template/src/layouts/Container.jsx
+++ Module5/my-react-template/src/layouts/Container.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import propTypes from "prop-types";
+import classNames from "classnames";
+import Header from "../components/Header";
 
-const Container = ({children}) => <div className="ui-container">{children}</div>;
+const Container = ({className, children, ...attrs}) => {
+    const classes = classNames("ui-container", className);
+    return (
+        <div className={classes} {...attrs}>{children}</div>
+    );
+}
+
+Container.propTypes = {
+    classNames: propTypes.string,
+    children: propTypes.node.isRequired
+}
+
+Container.defaultProps = {
+    classNames: ""
+}
 
 export default Container;
